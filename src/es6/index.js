0045fc2a36a7d1256f14bf96e43840c059491865
Omarvillada <olvillada@gmail.com>
2021-09-09 17:49:54 -0500

    Let, const, multilinea,spread operator y desestructuracion

--- src/es6/index.js
+++ src/es6/index.js
@@ -23,5 +23,40 @@ let epicPhrase = hello + ' ' + world
 console.log(epicPhrase)
 //Comillas francesas
 
-epicPhrase2 = `${hello} ${world}`
+epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+//Multilinea
+let lorem = "Quiero escribir poesia nadaísta \n"
++ "Otra frase sin sentido"
+
+//es6
+let lorem2 =  `Escribro y me falta una tecla
+programo y me falta memoria`;
+console.log(lorem)
+console.log(lorem2)
+
+//Desestructuracion de elementos
+
+let person = {
+    'name':'Omar',
+    'age': 24,
+     'country': 'COL'
+}
+console.log(
+    person.name,
+    person.age,
+    person.country
+)
+
+let { name } =  person
+console.log(name)
+
+//Operador de propagacion
+
+let team1 = ['Oscar', 'Omar', 'Julio']
+let team2 = ['Valeria', 'Valentina', 'Carolina']
+
+let education = ['David', ...team1, ...team2]
+
+console.log(education)
