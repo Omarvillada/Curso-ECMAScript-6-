32f111e9f86bfbf698de91a2f5063058da7dc355
Omarvillada <olvillada@gmail.com>
2021-09-10 12:01:56 -0500

    Asyn await

--- src/es8/index.js
+++ src/es8/index.js
@@ -7,3 +7,34 @@ const data = {
 console.log(data)
 const entries = Object.entries(data);
 console.log(entries);
+
+//Objetc Values: Me devuelve los valores de un objeto a un arreglo. 
+
+const data = {
+    frontend: 'Omar',
+    backend: 'Isabel',
+    desing: 'Ana'
+}
+const values = Object.values(data)
+console.log(values)
+// Padding: nos permite añadir cadenas vacías a string, pudiendo modificar la cadena string como tal.
+//Podría servir del lado del front , para mostrar una estructura de elementos.
+
+const string = 'hello' 
+console.log(string.padStart(7,'hi'))
+console.log(string.padEnd(12, ' ------'))
+
+
+const helloWorld = () =>{
+    return new Promise((resolve,reject) =>{
+        (true)
+        ? setTimeout(() => resolve('Hello World'), 3000)
+        : reject(new Error('Test error'))
+    })
+};
+
+const HelloAsyn =  async() =>{
+    const hello = await helloWorld();
+    console.log(hello)
+};
+HelloAsyn()
