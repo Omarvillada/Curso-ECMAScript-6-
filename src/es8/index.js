96500ce4cc95223541ef4431c446cbee90afc331
Omarvillada <olvillada@gmail.com>
2021-09-10 11:31:06 -0500

    Que se implemento en es8

---
+++ src/es8/index.js
@@ -0,0 +1,9 @@
+//Object entries deveule los valores de una matriz.
+const data = {
+    frontend: 'Omar',
+    backend: 'Isabel',
+    desing: 'Ana'
+}
+console.log(data)
+const entries = Object.entries(data);
+console.log(entries);
